f3fb4bf4f249cf6ae68e6aa02846f31acc2b98c6
hyemimi <hyemi7375@gmail.com>
2023-01-09 08:49:27 +0900

    start

---
+++ src/theme.ts
@@ -0,0 +1,14 @@
+import { DefaultTheme } from "styled-components";
+
+export const darktheme: DefaultTheme = {
+  bgColor: "#2f3640",
+  textColor: "#f5f6fa",
+  accentColor: "#CAD3C8",
+  hoverColor: "#e74c3c",
+};
+export const lighttheme: DefaultTheme = {
+  bgColor: "#CAD3C8",
+  textColor: "#f5f6fa",
+  accentColor: "#2f3640",
+  hoverColor: "#1abc9c",
+};
